5e3defdf7f3620e362ae87006cde1115dc22f06e
meiterk <go324@pm.me>
2026-02-19 16:21:08 +0300

    neuroslop test removal

--- src/MedAppServer/src/main/kotlin/org/kert0n/medappserver/db/repository/FormTypeRepository.kt
+++
@@ -1,9 +0,0 @@
-package org.kert0n.medappserver.db.repository
-
-import org.kert0n.medappserver.db.model.parsed.FormType
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface FormTypeRepository : JpaRepository<FormType, UUID> {
-    fun findByName(name: String): FormType?
-}
